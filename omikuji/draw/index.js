3cca0708296cccf4e1931dd49311536176b36f2e
JF1HNL <kimossan88@gmail.com>
2021-12-30 15:46:26 +0900

    update : おみくじ引くページ

--- omikuji/draw/index.js
+++ omikuji/draw/index.js
@@ -1,3 +1,5 @@
 function animation(){
   document.querySelector("#omikuji").classList.add("omikuji-animation")
+  document.querySelector("input").remove()
+  setTimeout(() => {window.location.href = "https://www.lovelive-anime.jp/"}, 10*1000 - 500)
 }
